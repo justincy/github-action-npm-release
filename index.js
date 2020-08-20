103b9b1b0c2a8cbd200f3e1e22d649307f205519
Justin York <jyork@genward.com>
2020-08-20 13:16:11 -0600

    Fix issue with package.json being bundled

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ async function main() {
   const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
 
   // 1. Get the current version
-  const {version: newVersion} = require(resolve('./package.json'));
+  const {version: newVersion} = __non_webpack_require__(resolve('./package.json'));
   core.info(`New version: ${newVersion}`);
 
   // 2. Get the latest release version and hash
